cf4fa14b12b6decbf7bf806e65874f7b6676a72b
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-10-03 12:28:00 +0530

    feat: added PrismaService and TaskModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TasksModule } from './tasks/tasks.module';
 import appConfig from './configs/app-config/app.config';
+import { PrismaService } from './configs/database-config/prisma.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [appConfig],
     }),
+    TasksModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
