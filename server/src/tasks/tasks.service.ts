bb2dc93fe57879e756ad1dcd424c9af35785982c
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-10-03 12:31:48 +0530

    feat: created api end point for create task

--- server/src/tasks/tasks.service.ts
+++ server/src/tasks/tasks.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+} from '@nestjs/common';
+import { PrismaService } from 'src/configs/database-config/prisma.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private readonly logger = new Logger(TasksService.name);
+  constructor(private prisma: PrismaService) {}
+
+  async createTask(dto: CreateTaskDto) {
+    try {
+      const task = await this.prisma.task.create({
+        data: dto,
+      });
+
+      return task;
+    } catch (error) {
+      this.logger.error(error);
+      if (error instanceof BadRequestException) {
+        throw error;
+      }
+      throw new InternalServerErrorException(error);
+    }
+  }
+}
